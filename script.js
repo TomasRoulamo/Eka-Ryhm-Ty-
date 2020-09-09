19921248e658f07706fc6bbf080eb4d89109fab0
unknown <lauri.savopirtti@edu.keuda.fi>
2020-09-09 10:27:05 +0300

    e

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function eka() {
+function eka() { // Etunimen tarkastus
   var eNimi = form.eNimi.value;
 
 if(eNimi.length<2)
@@ -9,24 +9,31 @@ return false;
 }
 }
 
-function toka() {
+function toka() { // Salasanan tarkastus
   var Passu = form.Passu.value;
 
   if(Passu.length<3)
   {
-  alert("Et antanut salasanaa!");
+  alert("Et antanut salasanaa tai se on liian lyhyt!");
   form.Passu.focus();
   return false;
   }
 }
 
-function () {
+function pelaaja() { //Radion pelaaja tarkastus
   var rVastaus=-1;
-
 for (var i=0; i < form.rVastaus.length;i++) {
 if (form.rVastaus[i].checked==true) {
 rVastaus=i;
-if(intVastaus==-1){
+if(rVastaus==-1){
 alert("Et valinnut vastataanko palautteeseesi!");
 return false;
+}
+
+function koulu() { //Radion koulu tarkistus
+
+}
+
+function boomer() { //Radion boomer tarkistus
+
 }
