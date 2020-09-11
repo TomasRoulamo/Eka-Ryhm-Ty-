eb5e08a002ab58e025adc8a2a1246470657a0be8
unknown <lauri.savopirtti@edu.keuda.fi>
2020-09-11 09:28:21 +0300

    qwertyuio

--- Projekti2/peli.js
+++ Projekti2/peli.js
@@ -1,28 +1,11 @@
-
 function Numero();
-<div>
-var num = Math.floor(Math.random() * 10 + 1);
-//guess variable to store the guessed number by user
-var arvaa;
-//output to store output to the user
-var output;
-//if the user guessed the number or not, initialize it to false
-var arvattu = false;
-var hintit = document.getElementById('hints');
-
-//do/while loop, while condition is based on if the user NOT guessing the number (e.g. guessed == false)
-do {
-  arvaa = prompt("Arvaa numero 1 ja 10 välillä");
-  hintit.textContent += "Arvasit numeron " + arvaa + '\n';
-  if (arvaa > num) {
-    elHints.textContent += "Arvasit liian suuren. Se on pienempi" + '\n';
-    arvattu = false;
-  } else if (arvaa < num) {
-    elHints.textContent += "Arvasit liian pienen. Se on isompi." + '\n';
-    arvattu = false;
-  } else {
-    alert("Arvasit oikein!")
-    arvattu = true;
-  }
-}
-while (arvattu === false);
+var yksi = document.getElementById('1')
+var kaksi = document.getElementById('2')
+var kolme = document.getElementById('3')
+var neljä = document.getElementById('4')
+var viisi = document.getElementById('5')
+var kuusi = document.getElementById('6')
+var seitsemän = document.getElementById('7')
+var kahdeksan = document.getElementById('8')
+var yhdeksän = document.getElementById('9')
+var kymmenen = document.getElementById('10')
