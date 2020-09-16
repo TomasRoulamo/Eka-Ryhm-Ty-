5d88128fabaaf739bf1bb6d1f8204db82872ca62
unknown <tomas.roulamo@edu.keuda.fi>
2020-09-16 09:31:21 +0300

    kokook

--- Projekti3/laskin.js
+++ Projekti3/laskin.js
@@ -1,10 +1,27 @@
+var luku1, luku2, luku3, luku4, luku5, luku6, luku7, luku8, luku9, luku0, miinus, plus, pilkku, jako, kerto;
+luku1= document.getElementById('1');
+luku2= document.getElementById('2');
+luku3= document.getElementById('3');
+luku4= document.getElementById('4');
+luku5= document.getElementById('5');
+luku6= document.getElementById('6');
+luku7= document.getElementById('7');
+luku8= document.getElementById('8');
+luku9= document.getElementById('9');
+luku0= document.getElementById('0');
+plus= document.getElementById('+');
+miinus= document.getElementById('-');
+jako= document.getElementById('/');
+kerto= document.getElementById('*');
+pilkku= document.getElementById(',');
+
 function clear(){
    window.location.reload();
 }
 
 function lisaa(){
 
-
+   
 }
 
 function laske(){
@@ -14,5 +31,5 @@ function laske(){
 
 function taakse(){
 
-      
+
 }
