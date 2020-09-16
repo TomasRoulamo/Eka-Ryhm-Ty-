6e33c67c5833e3616a11ab08eb418da212b472b2
unknown <tomas.roulamo@edu.keuda.fi>
2020-09-16 09:04:48 +0300

    fdsafdsafs

--- Projekti3/laskin.js
+++ Projekti3/laskin.js
@@ -1,19 +1,11 @@
-const laskin = {
-  displayValue: '0',
-  firstOperand: null,
-  waitingForSecondOperand: false,
-  operator: null,
-};
-
 function clear(){
 
 }
-
 
 function laske(){
    
 }
 
 function taakse(){
-   
+
 }
