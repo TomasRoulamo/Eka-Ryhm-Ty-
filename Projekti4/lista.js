c35c1b2dd0e2503f2010796e3856c2143416566a
unknown <lauri.savopirtti@edu.keuda.fi>
2020-09-16 12:39:47 +0300

    §11234yu

---
+++ Projekti4/lista.js
@@ -0,0 +1,6 @@
+function teksti(){
+  var solmu = document.createElement("LI");
+  var textisolmu = document.createTextNode("Water");
+  solmu.appendChild(textisolmu);
+  document.getElementById("myList").appendChild(solmu);
+}
