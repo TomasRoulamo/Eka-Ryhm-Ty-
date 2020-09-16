8c9516bd62140e3ad75da5e189231478936ece98
unknown <tomas.roulamo@edu.keuda.fi>
2020-09-16 11:25:13 +0300

    fdsafdsafdsafdsa

--- Projekti3/laskin.js
+++ Projekti3/laskin.js
@@ -1,24 +1,21 @@
 var nro = ' ';
-function lisaa(luku)
-{//luvun lisääminen
+
+function lisaa(luku){//luvun lisääminen
    nro += luku; 
    
    document.getElementById('tulos').innerHTML = nro;
-   //console.log('testi');
+   
 }
-/*
-function laske()
-{//Lasku funktio
+
+function laske(){//Lasku funktio
    nro = eval(nro);
-   document.getElementById('tulos') = nro;
+   document.getElementById('tulos').innerHTML = nro;
 }
 
-function taakse()
-{//Reverse funktio
+function taakse(){//Reverse funktio
 
 }
 
-function clear()
-{//Clear nappulalla saa päivitettyä koko sivun
+function clear(){//Clear nappulalla saa päivitettyä koko sivun
    window.location.reload();
-}*/
+}
