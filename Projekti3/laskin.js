393808b4e5a986b39356ec04c34d0686e868831f
unknown <tomas.roulamo@edu.keuda.fi>
2020-09-16 09:06:37 +0300

    dsgdsgdsag

--- Projekti3/laskin.js
+++ Projekti3/laskin.js
@@ -1,14 +1,5 @@
 function clear(){
 
-<<<<<<< HEAD
-    if(arvo != '+' || arvo != '-')
-    {
-        numero += ''+arvo+'';
-        document.getElementById('tulos').innerHTML = numero;
-       return;
-    }
- }
-=======
 }
 
 function laske(){
@@ -16,11 +7,5 @@ function laske(){
 }
 
 function taakse(){
-<<<<<<< HEAD
-   
-}
->>>>>>> 13a2a9ae52d2ee1aa3d6cc8e9a4535377e94b34d
-=======
 
 }
->>>>>>> 6e33c67c5833e3616a11ab08eb418da212b472b2
