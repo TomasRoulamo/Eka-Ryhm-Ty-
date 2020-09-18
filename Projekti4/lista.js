3d28bebd8896c473578a2cfc549f77afcf473341
Jeksku <tomas.roulamo@hotmail.com>
2020-09-18 09:08:34 +0300

    sasasa

--- Projekti4/lista.js
+++ Projekti4/lista.js
@@ -1,3 +1,5 @@
+var moi = event.which;
+
 function teksti(){
   var solmu = document.createElement("LI");
   var teksti = document.getElementById("boxi").value;
@@ -5,3 +7,22 @@ function teksti(){
   solmu.appendChild(tekstisolmu);
   document.getElementById("ekaLista").appendChild(solmu);
 }
+
+function painallusJalkeen()
+{
+    if(syottopituus() > 0) // tarkistetaan, onko edellä luotu funktion pituus > 0
+    {
+       teeElementti(); // jos on, käynnistetään oletusfunktio
+    }
+}
+
+function enterJalkeen()
+{
+    if(syottopituus() > 0 && event.which == 13) // event.which == 13 (enterin painallus)
+    {
+         teeElementti(); //käynnistetään oletusfunktio
+    }
+}
+
+enterButton.addEventListener("click", painallusJalkeen());
+listamuuttuja.addEventListener("keypress", enterJalkeen());
