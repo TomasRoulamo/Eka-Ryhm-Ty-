482ff7139100c9f6cda0cc79c8a57e74bc834ea2
unknown <lauri.savopirtti@edu.keuda.fi>
2020-09-21 10:49:29 +0300

    wsedfrtgujiko

--- Projekti4/lista.js
+++ Projekti4/lista.js
@@ -1,48 +1,15 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+function teksti(){
+  var muuttujaX = document.createElement("ekaLista");
+  var solmu = document.createElement("LI");
+  var teksti = document.getElementById("boxi").value;
+  var tekstisolmu = document.createTextNode(teksti);
+  solmu.appendChild(tekstisolmu);
+  document.getElementById("ekaLista").appendChild(solmu);
+  document.getElementById("ekaLista").classList.add("li")
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+  var boksi = document.getElementById("boxi").value;//Tarkistaa uuden tehtävän pituuden
+  if(boksi.length < 2);
+    {
+      alert('Et ole kirjoittanut lisättävää tehävää.');
     }
   }
-}
