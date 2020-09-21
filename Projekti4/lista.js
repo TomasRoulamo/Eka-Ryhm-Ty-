36320435104bd2d29160fa3b102901e61ff50768
unknown <lauri.savopirtti@edu.keuda.fi>
2020-09-21 09:35:42 +0300

    asdfghj

--- Projekti4/lista.js
+++ Projekti4/lista.js
@@ -8,14 +8,10 @@ function teksti(){
   solmu.appendChild(tekstisolmu);
   document.getElementById("ekaLista").appendChild(solmu);
   document.getElementById("ekaLista").classList.add("li")
-  
+
   var boksi = document.getElementById("boxi").value;//Tarkistaa uuden tehtävän pituuden
   if(boksi.length < 2);
     {
       alert('Et ole kirjoittanut lisättävää tehävää.');
     }
-<<<<<<< HEAD
-    muuttujaX.appendChild(document.createTextNode("X"));
-=======
   }
->>>>>>> a2886eb4a912fa4baa41344b4b3277bbd44b12e4
