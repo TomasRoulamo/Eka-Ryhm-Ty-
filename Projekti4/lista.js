238f750497784d52f7a16d7a8e4b4c38e6f53a76
unknown <tomas.roulamo@edu.keuda.fi>
2020-09-22 09:55:50 +0300

    fdsafs

--- Projekti4/lista.js
+++ Projekti4/lista.js
@@ -2,17 +2,23 @@ function teksti(){
   var solmu = document.createElement("LI");
   var teksti = document.getElementById("boxi").value;
   var tekstisolmu = document.createTextNode(teksti);
-  solmu.appendChild(tekstisolmu);
-  document.getElementById("ekaLista").appendChild(solmu);
-  document.getElementById("ekaLista").classList.add("li")
-
+  
+  
+  function teeListaHomma(){
+    solmu.appendChild(tekstisolmu);
+    document.getElementById("ekaLista").appendChild(solmu);
+    document.getElementById("ekaLista").classList.add("li")
+    return false;
+  }
   var boksi = document.getElementById("boxi").value;//Tarkistaa uuden tehtävän pituuden
-  if(boksi.length < 2);
+    if(boksi.length < 2);
     {
       alert('Et ole kirjoittanut lisättävää tehävää.');
+      return false;
     }
   }
   function crossOut() {
     li.addEventListener("click",crossOut);
-		li.classList.toggle("done");
+    li.classList.toggle("valmis");
+    return false;
 	}
