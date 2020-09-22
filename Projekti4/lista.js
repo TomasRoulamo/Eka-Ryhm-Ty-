4241b1ee457e0aa2e6037457affa12a5d00c0689
unknown <tomas.roulamo@edu.keuda.fi>
2020-09-22 10:58:49 +0300

    fdsafdsafsa

--- Projekti4/lista.js
+++ Projekti4/lista.js
@@ -4,28 +4,30 @@ function teksti(){
   var tekstisolmu = document.createTextNode(teksti);
   solmu.appendChild(tekstisolmu);
   document.getElementById("ekaLista").appendChild(solmu);
-
-
-  var li = document.createElement("li"); // luo "li" elementin
-  // Tarkistetaan, että on syötetty jotain
-  if(input.value != '')
-  {
-    li.appendChild(document.createTextNode(input.value)); //lisätään li-elementtiin tekstin, joka on laatikossa
-  	ul.appendChild(li); //lisää li-elementin ul-elementtiin
-  	input.value = ""; // Tyhjentää syöttökentän
   }
-    var poisto = document.createElement("button");
-poisto.appendChild(document.createTextNode("X"));
-li.appendChild(poisto);
-poisto.addEventListener("click", poistaTehtava);
 
-function poistaTehtava()
-{
-  li.classList.add("delete")
+  var ul = document.querySelector('ul');
+  var li = document.querySelector("li"); 
+
+  if(boxi.value == 0){
+    li.appendChild(document.createTextNode(boxi.value));
+  	ul.appendChild(li); 
+  	boxi.value = ""; 
+  }  
+  
+  var poisto = document.createElement("nappula");
+    poisto.appendChild(document.createTextNode("X"));
+    li.appendChild(poisto);
+    poisto.addEventListener("click", poistaTehtava);
+
+function poistaTehtava(){
+  li.classList.add("poistettu");
+  
 }
-  }
-  function crossOut() {
+
+
+function crossOut() {
     li.addEventListener("click",crossOut);
     li.classList.toggle("valmis");
-    return false;
 	}
+
