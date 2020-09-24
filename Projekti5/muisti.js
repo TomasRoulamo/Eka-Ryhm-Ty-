3409605ec5b16bddb4bbb3645ebeb56ac210535c
unknown <tomas.roulamo@edu.keuda.fi>
2020-09-24 09:37:36 +0300

    kkk

--- Projekti5/muisti.js
+++ Projekti5/muisti.js
@@ -1,8 +1,7 @@
 var kortit = ['tiku.png', 'Aku.png', 'shifu.png', 'karhu.png', 'lintu.jpg', 'mörkö.jpg', 'norsu.jpg', 'kissa.jpg'];
+kortit.setAttribute('click', testi)
 
+function testi(){
 
-
-
-
-
-
+    kortit.toggle('painettu');
+}
