5482b4bcd1de7cec5567fe62d3fa91d6275eed4d
unknown <tomas.roulamo@edu.keuda.fi>
2020-09-25 09:12:57 +0300

    ikk

--- Projekti5/muisti.js
+++ Projekti5/muisti.js
@@ -4,5 +4,24 @@ var vanha = -1;
 var vanhaAlt;
 
 function aloita(){
+  var alue = document.getElementById('peliAlue');
+  var taulu = document.createElement('table'); 
+  var tauluSis = document.createElement('tbody');
+
+  for(var i = 0; i < 4; i++){
+    var rivi = document.createElement('tr');
   
+
+  for(var j = 0; j < 4; j++){
+      var solu = document.createElement('td');
+      var sis = document.createTextNode('');
+      //solu.setAttribute('alt', sekoitetut[(vanhaKortti)]);
+      solu.setAttribute('id', 'solu' + vanhaKortti);
+      solu.setAttribute('name', vanhaKortti);
+      solu.setAttribute('onclick', 'painettu('+vanhaKortti+');');
+      vanhaKortti += 1;
+      solu.appendChild(sis);
+      rivi.appendChild(solu);
+    }
+  }
 }
