78426d3e3eb3278ea1cdc9667dd9c21f343f390a
unknown <lauri.savopirtti@edu.keuda.fi>
2020-09-29 08:54:35 +0300

    d

--- Projekti5/muisti.js
+++ Projekti5/muisti.js
@@ -6,7 +6,7 @@ var vanhaAlt;
 function aloita(){//Kun sivu avataan nii kaikki tässä tapahtuu
   sekoitetut = sekoita(kortit);
   var alue = document.getElementById('peliAlue');
-  var taulu = document.createElement('table'); 
+  var taulu = document.createElement('table');
   var tauluSis = document.createElement('tbody');
 
   for(var i = 0; i < 4; i++){
@@ -62,7 +62,7 @@ function sekoita(taulu){//Sekoittaa taulun heti alussa
 
 function odota(joku) {
   setTimeout(function(){
-     
+
       joku.innerHTML = '';
   }, 5000);
 }
