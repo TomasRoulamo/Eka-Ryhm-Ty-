2d1bca311d0cc7e5f175919b2dd6bdd0901d18b5
unknown <tomas.roulamo@edu.keuda.fi>
2020-10-01 09:56:20 +0300

    fdsfdsfds

---
+++ Projekti6/lomake3.js
@@ -0,0 +1,57 @@
+function laheta(){ //Lähetä nappulan funktio
+    
+    //Etunimen tarkastus
+    var eNimi = document.getElementById('etu').value;
+    if(eNimi.length < 3){
+        alert("Et antanut etunimeäsi!");
+        return false;
+    }
+    
+    //Sukunimen tarkastus
+    var sNimi = document.getElementById('suku').value;
+    if(sNimi.length < 4){
+        alert("Et antanut sukunimeäsi!");
+        return false;
+    }
+    
+     //Puhelimen tarkastus 
+     var pNro = document.getElementById('pnro').value; 
+     if(pNro.length < 10){
+         alert('Et antanut puhelinnumeroasi.');
+         return false;
+     }
+
+    //Sähköpostin tarkastus
+    var sPosti = document.getElementById('sposti').value;
+    var ehdot = /\S+@\S+/;
+        if(!ehdot.test(sPosti)){   
+        alert("Anna kunnon sähköposti");
+        return false;
+    }
+    
+     //Työnantajan / oppilaitoksen tarkastus 
+    var tanto = document.getElementById('tanto').value; 
+    if(tanto.length < 5){
+        alert('Et antanut työnantajasi tai oppilaitoksesi nimeä.');
+       return false;
+    }
+    
+    //Syntymäajan tarkastus
+    var spaiva = document.getElementById('spaiva').value; 
+    if(spaiva.length < 8){
+        alert('Et antanut syntymäpäivääsi.');
+        return false;
+    }
+
+     //Syntymäajan tarkastus
+     var rvalio = document.getElementById('boxi').value; 
+     if(rvalio.length < 2){
+         alert('Et antanut syntymäpäivääsi.');
+         return false;
+     }
+
+    //Jos kaikkiin kohtiin on vastattu niin kiitetään täyttäjää
+    else{
+        alert('Kiitos kun täytit lomakkeen. :)')
+    }
+}
