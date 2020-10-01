392c465b7bb3df73c2fab521e453521724cd1454
unknown <tomas.roulamo@edu.keuda.fi>
2020-10-01 09:39:35 +0300

    dsadasda

--- Projekti6/lomake3.js
+++ Projekti6/lomake3.js
@@ -1,82 +1,39 @@
-function laheta(lomake){ //Lähetä nappulan funktio
+function laheta(){ //Lähetä nappulan funktio
     
     //Etunimen tarkastus
-    var eNimi = lomake.eka.value 
+    var eNimi = document.getElementById('etu').value;
     if(eNimi.length < 3){
         alert("Et antanut etunimeäsi!");
-        lomake.eka.focus();
         return false;
     }
     
+    //Sukunimen tarkastus
+    var sNimi = document.getElementById('suku').value;
+    if(sNimi.length < 4){
+        alert("Et antanut sukunimeäsi!");
+        return false;
+    }
+    
+     //Puhelimen tarkastus 
+     var pNro = document.getElementById('pnro').value; 
+     if(pNro.length < 10){
+         alert('Et antanut puhelinnumeroasi.');
+         return false;
+     }
+
     //Sähköpostin tarkastus
-    var sPosti = lomake.toka.value;
+    var sPosti = document.getElementById('sposti').value;
     var ehdot = /\S+@\S+/;
         if(!ehdot.test(sPosti)){   
         alert("Anna kunnon sähköposti");
-        lomake.toka.focus();
         return false;
     }
     
-    //Iän tarkastus (kesken)
-    var ika = lomake.kolmas.value 
-    if(ika.length < 2){
-        alert('Et laittanut ikääsi, tai olet liian nuori.');
-        lomake.kolmas.focus();
-        return false;
-    }
-
-    //Pallukoitten tarkastus
-    var pallukka = lomake.suositus;
-    var painettu = false;
-        for (var i = 0; i < pallukka.length; i++) {
-        if (pallukka[i].checked==true) {
-           painettu = true;
-        }
-    }
-        if(painettu == false) {
-            alert('Et ole valinnut pallukkaa.');
-            return false;
-    }
-    
-    //Checkboxien tarkastus
-    var boxit = lomake.mielipiteet; 
-    var vastaus = false;
-        for (var k=0; k < boxit.length;k++) {
-        if (boxit[k].checked==true) {
-           vastaus = true;
-        }
-    }
-        if(vastaus == false){
-            alert('Et ole valinnut laatikkoa.');
-            return false;
-    }
-    
-    var lemppari2 = document.getElementById('aineet2');//Lempiaineen tarkastus
-        if(lemppari2 = ''){
-          alert('Et ole valinnut lempainetta.');
-          return false;
-        }
-
-    //Tekstiboxin tarkastus
-    var teksti = lomake.Teksti.value 
-    if(teksti.length < 10){
-        alert('Palautteesi täytyy olla vähintään 10 kirjainta pitkä.');
-        lomake.Teksti.focus();
-        return false;
-    }
-    
-    else{ //Jos kaikkiin kohtiin on vastattu, niin saa täyttäjä kiitoksen.
-    alert('Kiitos kun täytit lomakkeen! :) "Lomake tyhjenee nyt automaattisesti"');
-    }
-
-    
-
+     //Työnantajan / oppilaitoksen tarkastus 
+     var tanto = document.getElementById('tanto').value; 
+     if(tanto.length < 5){
+         alert('Et antanut työnantajasi tai oppilaitoksesi nimeä.');
+         return false;
+     }
 }
 
-function tyhjenna(){//Tällä tyhjennetään kaikki lomakkeessa oleva tieto.
-
-    var tyhj = '';
-    if (!confirm("Haluatko varmasti tyhjentää kaikki kentät?")) {//Tässä varmennetaan haluaako täyttäjä tyhjentää lomakkeen.
-    return false;
-    }
-}
