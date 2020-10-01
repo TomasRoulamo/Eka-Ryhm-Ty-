18eaf63f02e4fa5b550fc3768bcad1380d3abf2b
unknown <tomas.roulamo@edu.keuda.fi>
2020-10-01 08:54:50 +0300

    dsdds

---
+++ Projekti6/lomake3.js
@@ -0,0 +1,82 @@
+function laheta(lomake){ //Lähetä nappulan funktio
+    
+    //Etunimen tarkastus
+    var eNimi = lomake.eka.value 
+    if(eNimi.length < 3){
+        alert("Et antanut etunimeäsi!");
+        lomake.eka.focus();
+        return false;
+    }
+    
+    //Sähköpostin tarkastus
+    var sPosti = lomake.toka.value;
+    var ehdot = /\S+@\S+/;
+        if(!ehdot.test(sPosti)){   
+        alert("Anna kunnon sähköposti");
+        lomake.toka.focus();
+        return false;
+    }
+    
+    //Iän tarkastus (kesken)
+    var ika = lomake.kolmas.value 
+    if(ika.length < 2){
+        alert('Et laittanut ikääsi, tai olet liian nuori.');
+        lomake.kolmas.focus();
+        return false;
+    }
+
+    //Pallukoitten tarkastus
+    var pallukka = lomake.suositus;
+    var painettu = false;
+        for (var i = 0; i < pallukka.length; i++) {
+        if (pallukka[i].checked==true) {
+           painettu = true;
+        }
+    }
+        if(painettu == false) {
+            alert('Et ole valinnut pallukkaa.');
+            return false;
+    }
+    
+    //Checkboxien tarkastus
+    var boxit = lomake.mielipiteet; 
+    var vastaus = false;
+        for (var k=0; k < boxit.length;k++) {
+        if (boxit[k].checked==true) {
+           vastaus = true;
+        }
+    }
+        if(vastaus == false){
+            alert('Et ole valinnut laatikkoa.');
+            return false;
+    }
+    
+    var lemppari2 = document.getElementById('aineet2');//Lempiaineen tarkastus
+        if(lemppari2 = ''){
+          alert('Et ole valinnut lempainetta.');
+          return false;
+        }
+
+    //Tekstiboxin tarkastus
+    var teksti = lomake.Teksti.value 
+    if(teksti.length < 10){
+        alert('Palautteesi täytyy olla vähintään 10 kirjainta pitkä.');
+        lomake.Teksti.focus();
+        return false;
+    }
+    
+    else{ //Jos kaikkiin kohtiin on vastattu, niin saa täyttäjä kiitoksen.
+    alert('Kiitos kun täytit lomakkeen! :) "Lomake tyhjenee nyt automaattisesti"');
+    }
+
+    
+
+}
+
+function tyhjenna(){//Tällä tyhjennetään kaikki lomakkeessa oleva tieto.
+
+    var tyhj = '';
+    if (!confirm("Haluatko varmasti tyhjentää kaikki kentät?")) {//Tässä varmennetaan haluaako täyttäjä tyhjentää lomakkeen.
+    return false;
+    }
+}
