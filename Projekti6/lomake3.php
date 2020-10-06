59b387a22b1b966c29eaa2d9e1f17dc1a1f79bc1
unknown <tomas.roulamo@edu.keuda.fi>
2020-10-06 09:18:33 +0300

    fdsafsdaf

--- Projekti6/lomake3.php
+++ Projekti6/lomake3.php
@@ -1,23 +1,34 @@
 <?php
-$enimi = "etu";
-$snimi = "suku";
-$losoite = ""
-$palvelin = "fdb28.awardspace.net";
-$kayttaja = "3597339_sqlopiskelu";
-$salasana = "72MoippaLe71";
-$tietokanta = "3597339_sqlopiskelu";
+    $enimi = "etu";
+    $snimi = "suku";
+    $losoite = "osoite";
+    $puhnro = "pnro";
+    $postinro = "posnro"
+    $ptp = "kaupunki";
+    $palvelin = "fdb28.awardspace.net";
+    $kayttaja = "3597339_sqlopiskelu";
+    $salasana = "72MoippaLe71";
+    $tietokanta = "3597339_sqlopiskelu";
 
-// luodaan yhteys muuttujaan
-$link = mysqli_connect($palvelin,$kayttaja,$salasana,$tietokanta);
+    // luodaan yhteys muuttujaan
+    $link = mysqli_connect($palvelin,$kayttaja,$salasana,$tietokanta);
 
-// tarkistetaan saadanko yhteys
-if(mysqli_connect_error()){
+    // tarkistetaan saadanko yhteys
+    if(mysqli_connect_error()){
 
-die("Tietokantaan ei saatu yhteyttä");
+    die("Tietokantaan ei saatu yhteyttä");
 
-}
+    }
 
-$talletus = "INSERT INTO Osoite (Etunimi, Sukunimi, Lahiosoite, Postinumero, Postitoimipaikka)
- VALUES ('$enimi','$snimi','$losoite','$pnro','$ptp')";
+    $talletus = "INSERT INTO Osoite (Etunimi, Sukunimi, Lahiosoite, Kaupunki, Postinumero, Puhelin, Sähköposti, Työnantaja tai oppilaitos, Ammattinimike, Syntymäaika, Tapahtuman hintavaihtoehdot, Erikoisruokavalio) 
+    VALUES ( '$enimi','$snimi','$losoite','$puhnro','$ptp','')";
 
-?>
+    if (mysqli_query($link, $talletus)){
+    echo "Uudet tiedostot paivitetty";
+    }
+
+    else{
+    echo "Virhe: " . $talletus . "<br>" . mysqli_error($link);
+    }
+
+    ?>
